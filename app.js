ed2466ebffc2dfd930d425ff3109142bf523c445
chachaxw <867571123@qq.com>
2016-11-10 13:18:38 +0800

    update code

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const chat = require('./routes/chat');
 const users = require('./routes/users');
+const chat = require('./routes/chat');
+const WS = require('ws');
 
 const app = express();
-
-app.ready = server => {
-  chat.prepareSocketIO(server);
-};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', chat);
+app.use('/chat', chat);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
