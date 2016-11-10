07804ab6f5b76b1507788cf60666439f69ee7470
chachaxw <867571123@qq.com>
2016-11-10 18:02:26 +0800

    创建socket服务器

--- routes/chat.js
+++ routes/chat.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const url = require('url');
 const WebSocketServer = require('ws').Server;
 
 /* GET home page. */
@@ -11,13 +12,24 @@ router.webSocket = server => {
   const wss = new WebSocketServer({ server: server });
 
   wss.on('connection', function connection(ws) {
+    const location = url.parse(ws.upgradeReq.url, true);
 
-    ws.on('message', function incoming(message) {
-      console.log('收到信息: %s', message);
+    ws.on('message', (data, flags) => {
+      console.log('收到消息: ' + data, flags);
+      ws.send('消息已收到');
     });
 
-    ws.send('Halo Chacha, my name is World');
+    ws.on('open', () => {
+      console.log('connected');
+      ws.send('Socket connected', {mask: true});
+    });
+
+    // ws.on('close', function close() {
+    //   console.log('disconnected');
+    // });
+    ws.send('Socket 已创建成功');
   });
-}
+
+};
 
 module.exports = router;
